c74ded8b7ebc0fae2bee68261651aff83d8f2f3f
MohammadNAKnw <neyaznak@gmail.com>
2023-12-09 16:51:26 +0530

    prt02

--- App.js
+++ App.js
@@ -1,18 +1,5 @@
-/**
- *
- * <div id="parent">
- *      <div id="child">
- *         <h1>I'm h1 tag</h1>
- *         <h2>I'm h1 tag</h2>
- *      </div>
- * <div id="child2">
- *         <h1>I'm h1 tag</h1>
- *         <h2>I'm h1 tag</h2>
- *      </div>
- * </div>
- *
- * ReactElement(Object) => HTML(Browser Understands)
- */
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div", { id: "parent" }, [
     React.createElement("div", { id: "child" }, [
@@ -24,9 +11,7 @@ const parent = React.createElement("div", { id: "parent" }, [
       React.createElement("h2", {}, "I'm an h2 tag"),
     ]),
   ]);
-  
-  //JSX
-  
+
   console.log(parent); // object
   
   const root = ReactDOM.createRoot(document.getElementById("root"));
