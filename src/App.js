5c293d03282918e9fa4188dc11ad4612bd324626
MohammadNAKnw <neyaznak@gmail.com>
2023-12-15 00:02:34 +0530

    children routes and Outlet

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
 import Header from "./components/Header";
 import Body from "./components/Body";
@@ -9,27 +9,33 @@ import Contact from "./components/Contact";
 import Error from "./components/Error";
 
 const AppLayout = () => {
-    return (
-        <div className="app">
-            <Header/>
-            <Body/>
-        </div>
-    )
-}
+  return (
+    <div className="app">
+      <Header />
+      <Outlet />
+    </div>
+  );
+};
 
 const appRouter = createBrowserRouter([
   {
     path: "/",
     element: <AppLayout />,
-    errorElement: <Error/>
-  },
-  {
-    path: "/about",
-    element: <About />,
-  },
-  {
-    path: "/contact",
-    element: <Contact />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
+    errorElement: <Error />,
   },
 ]);
 
